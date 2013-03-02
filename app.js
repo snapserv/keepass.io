41c8784febac15ec39e6b075e3c9ffe972d734c2
NeoXiD <github@snapserv.net>
2013-03-02 14:20:35 +0100

    Database parsing complete
    KeePass databases will be now completely parsed (groups, entries, meta
    data, recycle bin...) Passwords are being decoded with the Salsa20
    stream cipher.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var keepassio = require('./lib');
+var keepassio = require('./lib'),
+    util = require('util');
 
 var db = new keepassio();
 db.setCredentials({
     password: '123456'
 });
 db.load('Test.kdbx', function(data) {
-
+    console.log(util.inspect(data, false, 10, true));
 });
