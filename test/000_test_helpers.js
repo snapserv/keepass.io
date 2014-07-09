3d92c4adb79bd64565c9941f9e1aa6dfcf54a6a7
Pascal Mathis <dev@snapserv.net>
2014-07-09 21:29:21 +0200

    Added test cases for simple database operations

--- test/000_test_helpers.js
+++ test/000_test_helpers.js
@@ -2,4 +2,8 @@ var path = require('path');
 
 exports.respath = function(resourceName) {
 	return path.join(__dirname, 'resources', resourceName);
-}
+};
+
+exports.tmppath = function(fileName) {
+	return path.join(__dirname, 'temp', fileName);
+};
---
+++ test/010_db_basic.js
@@ -0,0 +1,56 @@
+var should = require('should');
+var helpers = require('./000_test_helpers');
+var kpio = require('../lib');
+
+describe('Opening the example database', function() {
+	var dbPath = helpers.respath('000_example.kdbx');
+	var kfPath = helpers.respath('000_example.key');
+	var newDbPath = helpers.tmppath('010_new_example.kdbx');
+
+	it('with invalid credentials should throw a KpioDatabaseError', function(done) {
+		var db = new kpio.Database();
+		db.addCredential(new kpio.Credentials.Password('morpheus'));
+		db.loadFile(dbPath, function(err, api) {
+			console.log(api);
+			(function() {
+				if(err) throw err;
+			}).should.throw(kpio.Errors.Database);
+			return done();
+		});
+	});
+
+	describe('with valid credentials', function() {
+		var db = null, dbApi = null;
+
+		before(function() {
+			db = new kpio.Database();
+			db.addCredential(new kpio.Credentials.Password('nebuchadnezzar'));
+			db.addCredential(new kpio.Credentials.Keyfile(kfPath));
+		});
+
+		it('should not throw any errors', function(done) {
+			db.loadFile(dbPath, function(err, api) {
+				if(err) return done(err);
+				dbApi = api;
+				return done();
+			});
+		});
+
+		describe('and calling #saveFile()', function() {
+			it('with the same credentials should not throw any error', function(done) {
+				db.saveFile(newDbPath, function(err, api) {
+					return done(err);
+				});
+			});
+
+			it('with different credentials should not throw any error', function(done) {
+				db.resetCredentials();
+				db.addCredential(new kpio.Credentials.Password('morpheus'));
+
+				db.saveFile(newDbPath, function(err, api) {
+					return done(err);
+				});
+			});
+		});
+	});
+});
