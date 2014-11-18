dc2a00c0cd80a1a803b9124ab6efe1aa7422d5de
NeoXiD <dev@snapserv.net>
2014-11-18 10:05:40 +0000

    Added more tests for the basic API to increase test coverage

--- test/012_basic_api.js
+++ test/012_basic_api.js
@@ -40,12 +40,12 @@ describe('Opening the example database', function() {
 			
 			before(function() {
 				backupTree = db.getBasicApi().getGroupTree();
-			})
+			});
 			
-			it('with an empty object should throw an error', function() {
+			it('with an empty array should not throw any errors', function() {
 				(function() {
-					db.getBasicApi().setGroupTree({});
-				}).should.throw();
+					db.getBasicApi().setGroupTree([]);
+				}).should.not.throw();
 			});
 			
 			it('with a valid group tree object should not throw any errors', function() {
@@ -69,6 +69,26 @@ describe('Opening the example database', function() {
 			});
 		});
 		
+		describe('and then calling #setGroup()', function() {
+			var backupGroup = null;
+			
+			before(function() {
+				backupGroup = db.getBasicApi().getGroup('Pl5s4tAbm0eiyg1aOOr0/A==');
+			});
+			
+			it('with an inexistant UUID should throw an error', function() {
+				(function() {
+					db.getBasicApi().setGroup('does-not-exist-uuid', {});
+				}).should.throw(kpio.Errors.Api);
+			});
+			
+			it('with an existing UUID should not throw any errors', function() {
+				(function() {
+					db.getBasicApi().setGroup('Pl5s4tAbm0eiyg1aOOr0/A==', backupGroup);
+				}).should.not.throw();
+			});
+		});
+		
 		describe('and then calling #getEntries()', function() {
 			it('with an inexistant group UUID should throw an error', function() {
 				(function() {
@@ -82,5 +102,71 @@ describe('Opening the example database', function() {
 				}).should.not.throw();
 			});
 		});
+		
+		describe('and then calling #setEntries()', function() {
+			var backupEntries = null;
+			
+			before(function() {
+				backupEntries = db.getBasicApi().getEntries('Pl5s4tAbm0eiyg1aOOr0/A==');
+			});
+			
+			it('with an inexistant group UUID should throw an error', function() {
+				(function() {
+					db.getBasicApi().setEntries('does-not-exist-uuid', []);
+				}).should.throw(kpio.Errors.Api);
+			});
+			
+			it('with an existing group UUID and no entries should not throw any errors', function() {
+				(function() {
+					db.getBasicApi().setEntries('Pl5s4tAbm0eiyg1aOOr0/A==', []);
+				}).should.not.throw();
+			});
+			
+			it('with an existing group UUID and some entries should not throw any errors', function() {
+				(function() {
+					db.getBasicApi().setEntries('Pl5s4tAbm0eiyg1aOOr0/A==', backupEntries);
+				}).should.not.throw();
+			});
+		});
+		
+		describe('and then calling #findOrphanedEntries()', function() {
+			it('should return 0 entries', function() {
+				var entries = db.getBasicApi().findOrphanedEntries();
+				var count = 0;
+				for(var key in entries) count += entries[key].length;
+				
+				count.should.equal(0);
+			});
+			
+			describe('but deleting the whole tree with #setGroupTree() before', function() {
+				it('should not throw any errors', function() {
+					db.getBasicApi().setGroupTree([]);
+				});
+				
+				it('should return 3 entries', function() {
+					var entries = db.getBasicApi().findOrphanedEntries();
+					var count = 0;
+					for(var key in entries) count += entries[key].length;
+
+					count.should.equal(3);
+				});
+			});
+		});
+		
+		describe('and then calling #deleteOrphanedEntries()', function() {
+			it('should not throw any errors', function() {
+				db.getBasicApi().deleteOrphanedEntries();
+			});
+			
+			describe('and finally calling #findOrphanedEntries()', function() {
+				it('should return 0 entries', function() {
+					var entries = db.getBasicApi().findOrphanedEntries();
+					var count = 0;
+					for(var key in entries) count += entries[key].length;
+
+					count.should.equal(0);
+				});
+			});
+		})
 	});
 });
